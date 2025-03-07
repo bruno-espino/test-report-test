2e5172a87c51a0501583d76ba0d463d45ce313aa
Bruno <brunoespi7@gmail.com>
2025-03-07 16:26:27 -0300

    added workflow

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,7 +1,13 @@
+import { defineConfig } from 'cypress';
+import { GenerateCtrfReport } from 'cypress-ctrf-json-reporter';
+
 export default {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+        new GenerateCtrfReport({
+          on,
+      })
     },
   },
 };
